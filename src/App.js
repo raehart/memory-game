1a3117480bfce20857befe814d9a07a7ca481bca
RJ <13499408+raehart@users.noreply.github.com>
2022-02-14 13:13:54 -0800

    Created new GameGrid component & added elevation utility class to GameCard Setup CardGrid component with 20 cards.  Added a new variable to track card direction in Game Card Component. Changed App.css to App.scss & moved gameGrid.scss into components (where it belongs).

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import GameGrid from './components/GameGrid';
 
 function App() {
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './GameCard.scss';
 
-export default function GameCard() {
+// add state -front or back to card
+let cardDirection = "faceUp";
+
+//add on-click to card- toggle cardDirection state 
+
+export default function GameCard(props) {
   return (
     <div className="u-high game-card">
       <blaze-card>
-       <h2>Game Card</h2>
+        <h2>Card State:  {cardDirection}</h2>
       </blaze-card>
     </div>
   );
