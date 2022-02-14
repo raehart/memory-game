eccf86b03d77667338e29090ed16e660992c891b
RJ <13499408+raehart@users.noreply.github.com>
2022-02-13 19:50:53 -0800

    Setup blaze UI component library via CDN.  Created new basic card component.

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function GameCard() {
+  return (
+    <div className="GameCard">
+      <blaze-card>
+        <blaze-card-header>
+          <h2 className="c-heading u-xlarge">
+            Game Card 
+          </h2>
+        </blaze-card-header>
+      </blaze-card>
+    </div>
+  );
+}
