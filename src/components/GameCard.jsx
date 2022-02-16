9badae539c0205020e88cfb7250e02030dc947ad
RJ <13499408+raehart@users.noreply.github.com>
2022-02-15 19:39:58 -0800

    Added conditional routing to gameCard component, refactored card to use images.  Lowered number of total cards from 20 to 12.  Cleaned up Sass and made grid look nice.

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './GameCard.scss';
 
-// add state -front or back to card
-let cardDirection = "faceUp";
-
-//add on-click to card- toggle cardDirection state 
-
 export default function GameCard(props) {
-  return (
-    <div className="u-high game-card">
-      <blaze-card>
-        <h2>Card State:  {cardDirection}</h2>
-      </blaze-card>
-    </div>
-  );
+  let [cardDirection, setCardDirection] = useState("faceDown");
+
+  const flipCard = () => {
+    setCardDirection(cardDirection === 'faceDown' ? 'faceUp' : 'faceDown')
+  }
+  if(cardDirection === 'faceDown') {
+    return (
+      <div role="button" className="c-card u-high game-card face-down-card" tabIndex={0} 
+        onClick={flipCard}>
+        <img alt="fancy blue checkerd playing card back with a floral center" className="o-image" src="/img/cardback.jpg"></img>
+      </div>
+    );
+  } else {
+    return (
+      <div role="button" className="c-card u-high game-card" tabIndex={0} 
+        onClick={flipCard}>
+        <img alt="unsplash placeholder" className="o-image" src="https://source.unsplash.com/random/550x500"></img>
+      </div>
+    )} 
 }
--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -2,106 +2,67 @@ import React from 'react';
 import GameCard from './GameCard';
 import './GameGrid.scss'
 
-// Setup and loop over intinal grid - 20 cards - 2 sets of 10 matching pairs
+// setup state to meausre faceUP cards- make sure max is two & check for match when 2 
+
 let cards = [{
-  id: 1,
-  value: "",
-  symbol: ""
-},
-{
-  id: 2,
-  value: "",
-  symbol: ""
-},
-{
-  id: 3,
-  value: "",
-  symbol: ""
-},{
-  id: 4,
-  value: "",
-  symbol: ""
-},
-{
-  id: 5,
-  value: "",
-  symbol: ""
-},
-{
-  id: 6,
-  value: "",
-  symbol: ""
-},
-{
-  id: 7,
-  value: "",
-  symbol: ""
-},
-{
-  id: 8,
-  value: "",
-  symbol: ""
-},
-{
-  id: 9,
-  value: "",
-  symbol: ""
-},
-{
-  id: 10,
-  value: "",
-  symbol: ""
-},
-{
-  id: 11,
-  value: "",
-  symbol: ""
-},
-{
-  id: 12,
-  value: "",
-  symbol: ""
-},
-{
-  id: 13,
-  value: "",
-  symbol: ""
-},
-{
-  id: 14,
-  value: "",
-  symbol: ""
-},
-{
-  id: 15,
-  value: "",
-  symbol: ""
-},
-{
-  id: 16,
-  value: "",
-  symbol: ""
-},
-{
-  id: 17,
-  value: "",
-  symbol: ""
-},
-{
-  id: 18,
-  value: "",
-  symbol: ""
-},
-{
-  id: 19,
-  value: "",
-  symbol: ""
-},
-{
-  id: 20,
-  value: "",
-  symbol: ""
-}
+    id: 1,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 2,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 3,
+    value: "",
+    symbol: ""
+  },{
+    id: 4,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 5,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 6,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 7,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 8,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 9,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 10,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 11,
+    value: "",
+    symbol: ""
+  },
+  {
+    id: 12,
+    value: "",
+    symbol: ""
+  }
 ] 
 
 export default function GameGrid(props) {
